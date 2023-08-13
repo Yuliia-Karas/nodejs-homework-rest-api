a5b89ed118c1390030870e0c60cbe5d22af5bb68
Yuliia Karas <117581403+Yuliia-Karas@users.noreply.github.com>
2023-08-13 13:06:40 +0300

    step 3

--- server.js
+++ server.js
@@ -1,5 +1,27 @@
 const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// })
+
+// const DB_HOST =
+//   "mongodb+srv://YuliiaKar:wfneQ6oL1vmacZ0K@cluster0.j4yjgoy.mongodb.net/db-contacts?retryWrites=true&w=majority";
+// mongoose
+//   .connect(DB_HOST)
+//   .then(() => console.log("Database connection successful"))
+//   .catch((error) => console.log(error.message));
+
+
+
+  const DB_HOST =
+    "mongodb+srv://YuliiaKar:wfneQ6oL1vmacZ0K@cluster0.j4yjgoy.mongodb.net/db-contacts?retryWrites=true&w=majority";
+  mongoose
+    .connect(DB_HOST)
+    .then(() => {
+      app.listen(3000)
+    })
+    .catch(error => {
+      console.log(error.message);
+      process.exit(1);
+});
