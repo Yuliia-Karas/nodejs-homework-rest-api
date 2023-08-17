0f17ce2fe5adfddb6f51edf48442d14629866f9f
Yuliia Karas <117581403+Yuliia-Karas@users.noreply.github.com>
2023-08-17 22:41:51 +0300

    formating

--- server.js
+++ server.js
@@ -1,27 +1,16 @@
-
 const mongoose = require("mongoose");
-const app = require('./app')
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000")
-// })
+const app = require("./app");
 
-// const DB_HOST =
-//   "mongodb+srv://YuliiaKar:wfneQ6oL1vmacZ0K@cluster0.j4yjgoy.mongodb.net/db-contacts?retryWrites=true&w=majority";
-// mongoose
-//   .connect(DB_HOST)
-//   .then(() => console.log("Database connection successful"))
-//   .catch((error) => console.log(error.message));
-
-const {DB_HOST, PORT = 3000 } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 // mongoose.set('strictQuery', true);
 
-  mongoose
-    .connect(DB_HOST)
-    .then(() => {
-      app.listen(PORT);
-    })
-    .catch(error => {
-      console.error(error.message);
-      process.exit(1);
-});
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT);
+  })
+  .catch((error) => {
+    console.error(error.message);
+    process.exit(1);
+  });
