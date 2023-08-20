c519519f26e65ffd8e7bf1931843b512612738b6
Yuliia Karas <117581403+Yuliia-Karas@users.noreply.github.com>
2023-08-20 08:48:14 +0300

    step 2

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,52 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const { User } = require("../models/users");
-// const { HttpError, ctrlWrapper } = require("../helpers");
-const { ctrlWrapper } = require("../helpers");
+const { ctrlWrapper, HttpError } = require("../helpers");
+const { SECRET_KEY } = process.env;
 
 const register = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw HttpError(409, "Email in use");
+  }
 
-    const newUser = await User.create(req.body);
+    const hashPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({...req.body, password: hashPassword});
+
+  res.status(201).json({
+    password: newUser.password,
+    email: newUser.email,
+  });
+};
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw HttpError(401, "Email or password is wrong");
+    }
+
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if (!passwordCompare) {
+        throw HttpError(400, "Помилка від Joi або іншої бібліотеки валідації");
+    }
+
+    const payload = {
+        id: user._id,
+    };
+
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
     
-    res.status(201).json({
-        password: newUser.password,
-        email: newUser.email,
-    })
-}
+    res.json({
+        token,
+    });
+};
 
 module.exports = {
-    register:ctrlWrapper(register),
-}
+    register: ctrlWrapper(register),
+    login: ctrlWrapper(login),
+};
